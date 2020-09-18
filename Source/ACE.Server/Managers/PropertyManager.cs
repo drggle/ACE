bf608b4d1fb1cff0c71ead344a84fa9dd408f011
Ubuntu <ubuntu@ip-172-31-15-73.us-east-2.compute.internal>
2020-09-18 02:20:58 +0000

    moving gloom properties

--- Source/ACE.Server/Managers/PropertyManager.cs
+++ Source/ACE.Server/Managers/PropertyManager.cs
@@ -587,10 +587,10 @@ namespace ACE.Server.Managers
                 ("vendor_unique_rot_time", new Property<double>(300, "the number of seconds before unique items sold to vendors disappear")),
                 ("vitae_penalty", new Property<double>(0.05, "the amount of vitae penalty a player gets per death")),
                 ("vitae_penalty_max", new Property<double>(0.40, "the maximum vitae penalty a player can have")),
-                ("xp_modifier_percentlevel", new Property<double>(1.0, "scales the amount of xp rewarded as a percent of current level")),
-                ("xp_modifier_quest", new Property<double>(1.0, "scales the amount of quest xp received by players"))
                 ("void_pvp_modifier", new Property<double>(0.5, "Scales the amount of damage players take from Void Magic. Defaults to 0.5, as per retail. For earlier content where DRR isn't as readily available, this can be adjusted for balance.")),
                 ("xp_modifier", new Property<double>(1.0, "scales the amount of xp received by players"))
+                ("xp_modifier_percentlevel", new Property<double>(1.0, "scales the amount of xp rewarded as a percent of current level")),
+                ("xp_modifier_quest", new Property<double>(1.0, "scales the amount of quest xp received by players"))
                 );
 
         public static readonly ReadOnlyDictionary<string, Property<string>> DefaultStringProperties =
